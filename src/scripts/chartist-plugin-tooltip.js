d9444cbc5ef8bccc95550f42418b872ea69c57e0
Justin Doyle <jdoyle65@uwo.ca>
2017-10-06 17:20:42 -0300

    Fixes width/height being incorrect
    When toolTip width/height changes due to the text being altered by the tooltipFnc.

--- src/scripts/chartist-plugin-tooltip.js
+++ src/scripts/chartist-plugin-tooltip.js
@@ -112,6 +112,11 @@
 
         if(tooltipText) {
           $toolTip.innerHTML = tooltipText;
+          
+          // Calculate new width and height, as toolTip width/height may have changed with innerHTML change
+          height = $toolTip.offsetHeight;
+          width = $toolTip.offsetWidth;
+          
           setPosition(event);
           show($toolTip);
 
